32c75969d8e0c85d7d65e37b5bfc8dd272db2c43
NikarashiHatsu <yourlovelydev@gmail.com>
2021-09-17 17:04:19 +0700

    initial setup

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+// Laravel generated
+require('./bootstrap');
+
+// Breeze alpine
+require('alpinejs');
+
+// Bootstrap
+require('popper.js');
+require('datatables');
+require('datatables.net-bs4')
+
+window.$ = window.jQuery = require('jquery');
