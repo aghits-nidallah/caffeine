b1e654279f476a7bccaed692eef1e274f6648694
aghits-nidallah <shiroyukihatsu@gmail.com>
2021-09-21 04:45:38 +0700

    chore: add @tailwindcss/aspect-ratio dev dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,20 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                brown: {
+                    '50': '#ffdcc2',
+                    '100': '#ffd1ad',
+                    '200': '#ffc599',
+                    '300': '#eda268',
+                    '400': '#da7e37',
+                    '500': '#c06722',
+                    '600': '#a85311',
+                    '700': '#8f3e00',
+                    '800': '#713200',
+                    '900': '#522500',
+                }
+            }
         },
     },
 
@@ -25,5 +39,6 @@ module.exports = {
     plugins: [
         require('@tailwindcss/forms'),
         require('@tailwindcss/line-clamp'),
+        require('@tailwindcss/aspect-ratio'),
     ],
 };
